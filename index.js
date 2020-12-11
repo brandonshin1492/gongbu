55f0e5458a7be104537ce014d0530468f05413bb
Brandon Shin <brandonshin1492@berkeley.edu>
2020-12-11 01:25:12 -0800

    installed opus dependencies for voice; created new index.js for music bot functionality, basic_command_index.js holds the previous simple commands

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-const Discord = require('discord.js')
-const client = new Discord.Client()
-
-const config = require('./config.json')
-const command = require('./command')
-
-client.on('ready', () => {
-    console.log('The client is ready!')
-
-    command(client, 'ping', (message) => {
-        message.channel.send('pong!')
-    })
-})
-
-client.login(config.token)
+yeet
