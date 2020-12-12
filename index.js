270dbfca4f6c033f1518e3ba6a4ab3be95271227
Brandon Shin <brandonshin1492@berkeley.edu>
2020-12-11 17:37:52 -0800

    completed basic setup for music bot; installed necessary packages

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-yeet
+/*
+* Yeet
+*/
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const config = require('./config.json')
+
+const ytdl = require('ytdl-core');
+const streamOptions = {
+    seek: 0,
+    volume: 1
+}
+
+client.login(config.token)
+
+client.on('ready', () => {
+    console.log(`logged in as ${client.user.username}#${client.user.discriminator}`)
+})
+
+client.on('message', async message => {
+    // Ignore any messages that aren't from a guild
+    if (!message.guild) return;
+})
