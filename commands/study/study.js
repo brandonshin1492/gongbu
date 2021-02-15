dd7fbe1e4170793ed071fc63ac2001b28b7636f2
Brandon Shin <brandonshin1492@berkeley.edu>
2021-02-15 15:52:46 -0800

    updated postgresql connection/querying code

--- commands/study/study.js
+++ commands/study/study.js
@@ -5,57 +5,46 @@
 */
 
 const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
 
 module.exports = {
     name: 'study',
-    commands: ['study'],
-    expectedArgs: '<mode> // available modes are `mute` and `boot`',
-    minArgs: 0,
+    commands: ['study', 'studying'],
+    expectedArgs: '<action> // available actions are `lock` and `release',
+    minArgs: 1,
     maxArgs: 1,
     permissions: [],
     requiredRoles: ['bot test'],
     callback: (message, args) => {
         // FILL IN COMMAND CODE HERE
-        if (args.length == 0) {
-            // default command is deafen mode
-            args.push('mute');
-        }
-        
+        // init userid for easier reference
         const userid = message.member.id;
+
         // query database for user status
-        try {
-            var db, results;
-            pool.connect()
-                .then(client => {
-                    db = client;
-                    const user_status_query = `SELECT status FROM user_study_status WHERE id = '${userid}'`;
-                    db.query('SELECT * FROM user_study_status')
-                        .then(result => {
-                            results = (result) ? result.rows : null;
-                        })
-                        .catch(console.log('error querying from user_study_status'));
-                })
-                .catch(console.log('error connecting to db'));
-        } catch (err) {
-            console.error(err);
-            return;
-        }
+            
+        // create db connection
+        // create connection to database
+        const pool = new Pool({
+            connectionString: process.env.DATABASE_URL,
+            ssl: {
+            rejectUnauthorized: false
+            }
+        });
 
-        console.log(`results type: ${typeof(results)}`);
-        console.log(results);
-
-        if (args[0] == 'mute') {
-
-        } else if (args[0] == 'boot') {
-
-        } else {
-            message.channel.send('...');
-        }
+        pool.connect();
+        const debug_query = 'SELECT * FROM user_study_status';
+        const user_status_query = `SELECT status FROM user_study_status WHERE id = '${userid}'`;
+        pool.query(debug_query, (err, res) => {
+            if (err) throw err;
+            for (let row of res.rows) {
+                console.log(typeof(row));
+                console.log(JSON.stringify(row));
+            }
+            pool.end();
+        });
+        // db.query()
+        //     .then(result => {
+        //         results = (result) ? result.rows : null;
+        //     })
+        //     .catch(console.log('error querying from user_study_status'));
     }
 }
