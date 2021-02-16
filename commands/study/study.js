9b2a90dfd213f10ee63327532955aaad9fb02472
Brandon Shin <brandonshin1492@berkeley.edu>
2021-02-15 17:16:24 -0800

    implemented lock/release and automatic channel moving/user deafening functionality

--- commands/study/study.js
+++ commands/study/study.js
@@ -6,22 +6,47 @@
 
 const { Pool } = require('pg');
 
+// length of snowflake (used by ids) is set to 18
+const SF_LEN = 18;
+
 module.exports = {
     name: 'study',
     commands: ['study', 'studying'],
-    expectedArgs: '<action> // available actions are `lock` and `release',
-    minArgs: 1,
+    expectedArgs: '<action> // available actions are `lock` and `release`. make sure both you and the bot are in a voice channel',
+    minArgs: 0,
     maxArgs: 1,
     permissions: [],
     requiredRoles: ['bot test'],
     callback: (message, args) => {
         // FILL IN COMMAND CODE HERE
-        // init userid for easier reference
-        const userid = message.member.id;
+        // default arg is lock
+        if (args.length == 0) {
+            args.push('lock');
+        }
 
-        // query database for user status
-            
-        // create db connection
+        if (args[0] != 'lock' || args[0] != 'release') {
+            message.channel.send('invalid args, need to pass `lock` or `release`');
+            return;
+        }
+
+        // init userid and guildid for easier reference
+        const member = message.member;
+        const userid = message.member.id;
+        const guildid = message.guild.id;
+
+        // bot must be in a voice channel before calling this command
+        const vcons = message.client.voice.connections;
+        const curr_vcon = vcons.find(vcon => vcon.voice.guild.id === message.guild.id);
+        if (!curr_vcon) {
+            message.channel.send('add me to a voice channel first, then try again!');
+            return;
+        }
+
+        // user must also be in a voice channel
+        if (!message.member.voice.channel) {
+            message.channel.send('join a voice channel first, then try again!');
+        }
+
         // create connection to database
         const pool = new Pool({
             connectionString: process.env.DATABASE_URL,
@@ -31,20 +56,52 @@ module.exports = {
         });
 
         pool.connect();
-        const debug_query = 'SELECT * FROM user_study_status';
-        const user_status_query = `SELECT status FROM user_study_status WHERE id = '${userid}'`;
-        pool.query(debug_query, (err, res) => {
-            if (err) throw err;
-            for (let row of res.rows) {
-                console.log(typeof(row));
-                console.log(JSON.stringify(row));
-            }
-            pool.end();
+
+        // check if table for server exists; if not, create one
+        // EDIT: can use CREATE TABLE IF NOT EXISTS <name> <cols>
+        const table_name = `${guildid}-study`
+        const table_create_query = `CREATE TABLE IF NOT EXISTS study_status.${table_name} (uid char(${SF_LEN}), status varchar(10), prev_channel char(${SF_LEN}))`
+        pool.query(table_create_query, (err, res) => {
+            if (err) console.log('table creation error'); throw err;
         });
-        // db.query()
-        //     .then(result => {
-        //         results = (result) ? result.rows : null;
-        //     })
-        //     .catch(console.log('error querying from user_study_status'));
+
+        // create query that will (1) insert user if they don't have a row, and (2) update their study status
+        // EDIT: can use INSERT ___ ON CONFLICT DO UPDATE
+        var insertion_query;
+        if (args[0] == 'lock') {
+            // update the table with the user's new status and prev voice channel
+            pool.query(`INSERT INTO study_status.${table_name} VALUES ('${userid}', 'studying', '${member.voice.channel}') ON CONFLICT DO UPDATE`, (err, res) => {
+                if (err) { console.log('lock status error'); throw err; }
+            });
+
+            // move the user into the "study zone", AKA the channel that the bot is currently in (`curr_vcon`), and deafen them
+            member.voice.setChannel(curr_vcon.channel);
+            member.voice.setDeaf(true, 'deafened for studying');
+
+        } else if (args[0] == 'release') {
+            // verify the user is currently studying/has a record in the table; if so, move back to prev channel
+            pool.query(`SELECT status, prev_channel FROM study_status.${table_name} WHERE uid = '${userid}'`, (err, res) => {
+                if (err) { message.channel.send('you arent currently studying'); return; }
+                else if (!res) { console.log('could not find that user; aborting'); return; }
+                else if (res.status !== 'studying') { message.channel.send('you arent currently studying'); return; }
+                else {
+                    // undeafen the user, and move them back to their previous channel
+                    member.voice.setDeaf(false, 'done studying');
+                    member.voice.setChannel(res.prev_channel);
+                }
+            });
+
+            // update the table to reflect the users new status
+            pool.query(`INSERT INTO study_status.${table_name} VALUES ('${userid}', 'free', null) ON CONFLICT DO UPDATE`, (err, res) => {
+                if (err) { console.log('release status error'); throw err; }
+            });
+        } else {
+            console.log("HOW DID THIS EVEN HAPPEN??? args values need to be checked more rigorously");
+            throw err;
+        }
+        
+        pool.end();
+
+        message.channel.send('done');
     }
 }
